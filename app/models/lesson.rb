1971aa15faa389e1fdf072a58d7f231b0dd34a68
Basia Madej <barbara.madej@gmail.com>
2013-06-26 13:49:51 +0200

    default order of things

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,9 +2,9 @@ class Lesson < ActiveRecord::Base
   attr_accessible :position, :clue, :draft, :title, :body, :goal, :visible
   scope :recent, order("created_at DESC")
   scope :visible, where("visible = ?", true)
+  default_scope order("position ASC")
 
   def next
-    Lesson.visible.order("position ASC").where("position > ?", self.position).first ||
-    Lesson.visible.order("position ASC").first
+    Lesson.visible.where("position > ?", self.position).first || Lesson.visible.first
   end
 end
