b8f5f27f8a0ffc335eda586c9e5a4913a780e067
Basia Madej <barbara.madej@gmail.com>
2014-09-03 23:11:52 +0200

    small changes for Rails 4

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,11 +1,16 @@
 class Answer < ActiveRecord::Base
-
   belongs_to :student
   belongs_to :lesson
   after_initialize :copy_draft
+  attr_reader :restart
 
   def copy_draft
     self.body = self.lesson.draft unless self.body.present?
   end
+
+  def restart=(value)
+    self.body = nil if value.present?
+  end
+
 
 end
