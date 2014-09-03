16e289f15b8a85d5c183d94d75557ead61a63733
Basia Madej <barbara.madej@gmail.com>
2014-09-03 19:26:59 +0200

    cleaning up and git ignore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.tools.min.js
-//= require jquery.scrollTo-min
-//= require codemirror/codemirror
+//= require jquery.scrollTo-min.js
+//= require codemirror/codemirror.js
 //= require codemirror/mode/css/css
 //= require custom
