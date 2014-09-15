cdf0a7dbd981e63e761827934636d7a18fcbe431
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-09-15 21:21:38 +0200

    removed obsolete classes, pieces of code

--- app/models/student.rb
+++
@@ -1,8 +0,0 @@
-class Student < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  scope :recent, -> { order("created_at DESC") }
-
-  has_many :answers
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 AmusingCss::Application.routes.draw do
-
-  resources :answers, only: [:create, :update]
-
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :students
 
   resources :lessons, only: [:show, :index]
 
