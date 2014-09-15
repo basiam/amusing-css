7f133527d80baf4a207d6d71fd588d4a8c643985
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-09-15 18:03:17 +0200

    skipping authentication, removed @answer related code for lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,9 @@
 class LessonsController < ApplicationController
-  before_filter :authenticate_student!, :only => [:show]
   def index
     @lessons = Lesson.visible.order("position ASC").all
   end
 
   def show
     @lesson = Lesson.visible.find(params[:id])
-    @answer = Answer.find_or_initialize_by(student_id: current_student.id, lesson_id: @lesson.id)
   end
 end
